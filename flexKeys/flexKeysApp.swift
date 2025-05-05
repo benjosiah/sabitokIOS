672478c18c3dfe7154ca477e24f34eba588d6abd
BenJosiah <benjosiah90@gmail.com>
2025-05-05 19:01:41 +0100

    Initial Commit

---
+++ flexKeys/flexKeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  flexKeysApp.swift
+//  flexKeys
+//
+//  Created by user on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct flexKeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
